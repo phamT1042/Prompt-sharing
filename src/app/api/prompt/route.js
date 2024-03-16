3d7ae3ae47e428e59e5d22bce2127a3b34575f0f
VanTienDesktop <pvantien203@gmail.com>
2024-03-16 11:28:27 +0700

    Fix feed being static

--- src/app/api/prompt/route.js
+++ src/app/api/prompt/route.js
@@ -1,6 +1,13 @@
 import { connectToDB } from "@/utils/database"
 import Prompt from "@/models/prompt"
 
+// Lỗi khi deploy feed component vẫn static khi build, không thay đổi sau khi
+// ta thêm prompt hoặc chỉnh sửa
+// Fix: https://stackoverflow.com/questions/76356803/data-not-updating-when-deployed-nextjs13-app-on-vercel-despite-using-cache-no
+// Specifically you can export the dynamic variable with the value of 'force-dynamic'
+// This is functionally similar to adding export const revalidate = 0
+export const dynamic = 'force-dynamic';
+
 export const GET = async (req) => {
     try {
         await connectToDB()
