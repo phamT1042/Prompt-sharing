e527ff1115e252dee7a9c06a7d972b219b299edb
VanTienDesktop <pvantien203@gmail.com>
2024-03-16 11:15:15 +0700

    Fix feed component

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -9,12 +9,12 @@ const PromptCardList = ({ data, handleTagClick }) => {
         <div className='mt-16 prompt_layout'>
             {
                 data.map(post => (
-                        <PromptCard
-                            key={post._id}
-                            post={post}
-                            handleTagClick={handleTagClick}
-                        />
-                    )
+                    <PromptCard
+                        key={post._id}
+                        post={post}
+                        handleTagClick={handleTagClick}
+                    />
+                )
                 )
             }
         </div>
@@ -25,13 +25,16 @@ const Feed = () => {
     const [searchText, setSearchText] = useState('')
     const [posts, setPosts] = useState([])
 
+    const fetchPosts = async () => {
+        const response = await fetch("/api/prompt");
+        const data = await response.json();
+
+        setAllPosts(data);
+    };
+
     useEffect(() => {
-        (async () => {
-            const response = await fetch('/api/prompt')
-            const data = await response.json()
-            setPosts(data)
-        })()
-    }, [])
+        fetchPosts();
+    }, []);
 
     const handleSearchChange = (e) => {
 
@@ -53,7 +56,7 @@ const Feed = () => {
 
             <PromptCardList
                 data={posts}
-                handleTagClick={() => {}}
+                handleTagClick={() => { }}
             />
         </section>
     )
